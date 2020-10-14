c9ac1eddbec6fa823d0a52f86bc6e0e0e8b40eb6
Arne <a.vaneycken@gmail.com>
2020-10-14 09:49:54 +0200

    css

--- src/main/java/be/vdab/frida/controllers/IndexController.java
+++ src/main/java/be/vdab/frida/controllers/IndexController.java
@@ -4,22 +4,34 @@ package be.vdab.frida.controllers;
 import be.vdab.frida.domain.Adres;
 import be.vdab.frida.domain.Gemeente;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    private static final int EEN_JAAR_IN_SECONDEN = 31_536_000;
     @GetMapping
-    public ModelAndView index(){
+    public ModelAndView index(@CookieValue(name = "reedsBezocht", required = false)
+                                      String reedsBezocht, HttpServletResponse response) {
         var openGesloten = LocalDate.now().getDayOfWeek() == DayOfWeek.MONDAY ?
                 "gesloten" : "open";
         var modelAndView = new ModelAndView("index","openGesloten",openGesloten);
         modelAndView.addObject("adres", new Adres("Fridastraat","10",new Gemeente("Fridastad",1234)));
+        var cookie = new Cookie("reedsBezocht", "ja");
+        cookie.setMaxAge(EEN_JAAR_IN_SECONDEN);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        if (reedsBezocht != null) {
+            modelAndView.addObject("reedsBezocht", true);
+        }
         return modelAndView;
     }
 }
