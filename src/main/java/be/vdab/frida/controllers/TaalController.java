42b5dc3f33ad1cbb232289cb34ed13b3ef6b856c
Arne <a.vaneycken@gmail.com>
2020-10-14 08:56:27 +0200

    css

---
+++ src/main/java/be/vdab/frida/controllers/TaalController.java
@@ -0,0 +1,17 @@
+package be.vdab.frida.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("talen")
+class TaalController {
+    @GetMapping
+    public ModelAndView nederlands(@RequestHeader("Accept-Language") String language) {
+        var modelAndView = new ModelAndView("taal");
+        return new ModelAndView("taal", "nederlands", language.startsWith("nl"));
+    }
+}
