ce8148a1f2e7605ac9b9ad743bf5533e9b2713ef
Arne <a.vaneycken@gmail.com>
2020-10-20 08:46:58 +0200

    unable to resolve

--- src/main/java/be/vdab/frida/repositories/JdbcSnackRepository.java
+++ src/main/java/be/vdab/frida/repositories/JdbcSnackRepository.java
@@ -3,22 +3,26 @@ package be.vdab.frida.repositories;
 import be.vdab.frida.domain.Snack;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class JdbcSnackRepository implements SnackRepository {
     private final JdbcTemplate template;
-    private final RowMapper<Snack> snackRowMapper = (result, rowNum) -> new Snack(
-            result.getLong("id"), result.getString("naam"), result.getBigDecimal("prijs"));
+    private final RowMapper<Snack> snackRowMapper = (result, rowNum) ->
+            new Snack(result.getLong("id"),
+                      result.getString("naam"),
+                      result.getBigDecimal("prijs"));
     public JdbcSnackRepository(JdbcTemplate template) {
         this.template = template;
     }
 
     @Override
     public Optional<Snack> findById(long id) {
-        var sql ="select id,naam,prijs from snacks where id =?";
-        return Optional.of(template.queryForObject(sql,snackRowMapper,id));
+        var sql = "select id,naam,prijs from snacks where id =?";
+        return Optional.of(template.queryForObject(sql, snackRowMapper, id));
     }
 
     @Override
