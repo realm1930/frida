926c28414274aab03ddb5e961d5a1d3b19a50d9f
Arne <a.vaneycken@gmail.com>
2020-10-28 19:51:57 +0100

    gastenboek begin

--- src/main/java/be/vdab/frida/forms/GastenBoekEntryForm.java
+++ src/main/java/be/vdab/frida/forms/GastenBoekEntryForm.java
@@ -2,12 +2,10 @@ package be.vdab.frida.forms;
 
 import be.vdab.frida.domain.GastenBoekEntry;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class GastenBoekEntryForm extends GastenBoekEntry {
-    public GastenBoekEntryForm(long id, @NotBlank String naam, @NotNull LocalDate datum, @NotBlank String bericht) {
+    public GastenBoekEntryForm(String naam, String bericht) {
         super(0,naam,LocalDate.now(),bericht);
     }
 }
