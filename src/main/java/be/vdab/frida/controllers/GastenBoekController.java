8aa00c6678cebcf81e493701ecda2a2c7c0f4e05
Arne <a.vaneycken@gmail.com>
2020-10-30 09:28:03 +0100

    gastenboek begin

--- src/main/java/be/vdab/frida/controllers/GastenBoekController.java
+++ src/main/java/be/vdab/frida/controllers/GastenBoekController.java
@@ -1,10 +1,15 @@
 package be.vdab.frida.controllers;
 
+import be.vdab.frida.forms.GastenBoekEntryForm;
 import be.vdab.frida.services.GastenBoekService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("gastenboek")
@@ -18,5 +23,19 @@ public class GastenBoekController {
     public ModelAndView findAll(){
         return new ModelAndView("gastenboek",
                 "gastenboek",gastenBoekService.findAll());
+    }
+    @GetMapping("toevoegen/form")
+    public ModelAndView toevoegForm() {
+        return new ModelAndView("gastenboek", "gastenboek",
+                gastenBoekService.findAll())
+                .addObject(new GastenBoekEntryForm("", ""));
+    }
+    @PostMapping("toevoegen")
+    public ModelAndView toevoegen(@Valid GastenBoekEntryForm form, Errors errors){
+        if (errors.hasErrors()) {
+            return new ModelAndView("gastenboek", "gastenboek", gastenBoekService.findAll());
+        }
+        gastenBoekService.create(form);
+        return new ModelAndView("redirect:/gastenboek");
     }
 }
